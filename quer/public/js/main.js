47f4c500b4a37f079f88fea5e4ddc13bed3746b9
rowanvanekeren <rowanvanekeren@hotmail.com>
2016-10-07 19:11:52 +0200

    achtergrond animatie

--- quer/public/js/main.js
+++ quer/public/js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
 $(".datepicker").datepicker({ dateFormat: 'dd-mm-yy' });
 
+    var i=1;
+    $('.home_banner').css('background-image','url(./images/homepage_banner/party_1.jpg)');
+    setInterval(function(){
+
+            $('.home_banner').css('background-image','url(./images/homepage_banner/party_'+i+'.jpg)');
+
+        if(i <= 4){
+            i++;
+        }else{
+            i=1;
+        }
+    },6000);
 });
