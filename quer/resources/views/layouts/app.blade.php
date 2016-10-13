1bddf6fd1f0c6f66a61840d585f28ccea4e7eaa8
rowanvanekeren <rowanvanekeren@hotmail.com>
2016-10-13 13:18:35 +0200

    push

--- quer/resources/views/layouts/app.blade.php
+++ quer/resources/views/layouts/app.blade.php
@@ -34,10 +34,34 @@
 
 <div id="mySidenav" class="sidenav">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    <a href="#">About</a>
-    <a href="#">Services</a>
-    <a href="#">Clients</a>
-    <a href="#">Contact</a>
+    <div class="inner-nav-content">
+
+        @if (Auth::guest())
+            <a href="{{ url('/login') }}">Login</a>
+            <a href="{{ url('/register') }}">Register</a>
+        @else
+            <div class="image-wrapper">
+                <ul>
+                    <li> <img class="user-image" src="./images/profiles/{{ Auth::user()->image }}"></li>
+                    <li><a href="{{ url('/dashboard') }}">Hallo {{ Auth::user()->username }}!</a></li>
+                </ul>
+            </div>
+            <a href="{{ url('/dashboard') }}">Dashboard</a>
+            <a href="{{ url('/logout') }}"
+               onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                Logout
+            </a>
+
+            <form id="logout-form" action="{{ url('/logout') }}" method="POST">
+                {{ csrf_field() }}
+            </form>
+        @endif
+        {{--  <a href="#">About</a>
+          <a href="#">Services</a>
+          <a href="#">Clients</a>
+          <a href="#">Contact</a>--}}
+    </div>
 </div>
 </nav>
     <div id="main">
@@ -49,10 +73,29 @@
 
         </ul>
         <ul class="sub-nav-right">
-            <li> <a href="#">About</a></li>
-            <li> <a href="#">Services</a></li>
-            <li> <a href="#">Clients</a></li>
-            <li> <a href="#">Contact</a></li>
+            @if (Auth::guest())
+                <li><a href="{{ url('/login') }}">Login</a></li>
+                <li><a href="{{ url('/register') }}">Register</a></li>
+
+            @else
+                <li>
+                    <a href="{{ url('/dashboard') }}">Dashboard</a>
+                </li>
+                <li>
+                    <a href="{{ url('/logout') }}"
+                       onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+
+                    <form id="logout-form" action="{{ url('/logout') }}" method="POST">
+                        {{ csrf_field() }}
+                    </form>
+                </li>
+
+
+
+            @endif
 
         </ul>
             <div class="burger-wrapper">
