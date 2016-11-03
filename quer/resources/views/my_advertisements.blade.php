f85d6e687fbd88ed7fd28852c6e9bf259d44d05d
rowanvanekeren <rowanvanekeren@hotmail.com>
2016-11-03 16:30:29 +0100

    my adverts

--- quer/resources/views/my_advertisements.blade.php
+++ quer/resources/views/my_advertisements.blade.php
@@ -9,11 +9,11 @@
    <div class="variable_content">
      
        @include('layouts.dashboard_menu')
-      
+      <div class="my_adv_page_title">
        <h1>Mijn advertenties</h1>
        <p>Hier heb je een overzicht van je bestaande advertenties.</p>
-       
 
+      </div>
        {{--
        @foreach ($advertisements as $advertisement)
        <div>
@@ -23,36 +23,46 @@
            <div><a href="{{url('/quer_overview/'.$advertisement->advertisement->id)}}">{{ $advertisement->amount_of_quers }}</a></div>
        </div>
        @endforeach
+
        --}}
-       @foreach ($advertisements as $advertisement)
        <div class="my_advert_wrapper">
-           <div class="my_advert_left">
-               <div class="amount_quers"><h1>{{ $advertisement->amount_of_quers }} Que'rs</h1></div>
-               <h1>Beschrijving</h1>
-               <p>{{ $advertisement->advertisement->private_description }}</p>
-           </div>
-           <div class="my_event_right" style="background-image: url('../public/images/homepage_banner/party_3.jpg')">
-               <h1>{{ $advertisement->advertisement->event->name }}</h1>
-           </div>
-           <div class="my_advert_footer"> <h1>{{ $advertisement->amount_of_quers }} Que'rs</h1></div>
+           <table class="my_advert_quers_left" >
+               <tr><th>{{--aantal quers--}}</th></tr>
+               @foreach ($advertisements as $advertisement)
+                   <tr> <td><a href="{{url('/quer_overview/'.$advertisement->advertisement->id)}}">{{ $advertisement->amount_of_quers }}</a>
+                           <p> Que'rs</p>
 
+                       </td>
+
+                   </tr>
+
+               @endforeach
+
+           </table>
+
+           <table class="my_advert_right" >
+               <tr>
+
+                   <th>Evenement</th>
+                   <th>Advertentie beschrijving</th>
+                   <th>Datum</th>
+                   <th>Aantal que'rs</th>
+               </tr>
+               @foreach ($advertisements as $advertisement)
+
+                   <tr>
+
+                       <td><div class="my_advert_right_max_height">{{ $advertisement->advertisement->event->name }} </div></td>
+                       <td><div class="my_advert_right_max_height">{{ $advertisement->advertisement->private_description }}</div></td>
+                       <td>{{ $advertisement->advertisement->event->date_event }}</td>
+                       <td><a href="{{url('/quer_overview/'.$advertisement->advertisement->id)}}">{{ $advertisement->amount_of_quers }}</a></td>
+                   </tr>
+
+               @endforeach
+           </table>
        </div>
-       @endforeach
-       <table class="my_advertisements">
-           <tr>
-               <th>Datum</th>
-               <th>Evenement</th>
-               <th>Aantal que'rs</th>
-           </tr>
-           @foreach ($advertisements as $advertisement)
-           <tr>
 
-               <td>{{ $advertisement->advertisement->event->date_event }}</td>
-               <td>{{ $advertisement->advertisement->event->name }}</td>
-               <td><a href="{{url('/quer_overview/'.$advertisement->advertisement->id)}}">{{ $advertisement->amount_of_quers }}</a></td>
-           </tr>
-           @endforeach
-       </table>
+
        
        
        
