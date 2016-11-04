cdb48cf1eafb82661d96a8506def9405c0141c90
Sarah <sarah.jehin@belgacom.net>
2016-11-04 20:31:22 +0100

    dashboard home

--- quer/resources/views/dashboard.blade.php
+++ quer/resources/views/dashboard.blade.php
@@ -8,7 +8,16 @@
      
 
       
-       <h1>Dashboard</h1>
+       <h1>Welkom {{ Auth::user()->first_name }}</h1>
+       
+       <div>
+           Wat kan je allemaal doen op dit dashboard?
+           <div>
+               <ul>
+                   <li>Bij je advertenties kan je zien hoeveel Que'rs zich hebben aangemeld voor jouw advertenties.</li>
+               </ul>
+           </div>
+       </div>
        <p>Hierop kan je je huidige contracten, openstaande advertenties, ... zien</p>
       
        <p>Onderstaand dashboard menu moet ook in een soort layout staan die ge-extend kan worden</p>
