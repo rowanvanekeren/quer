127f27c5959752b26d9015d0f7b8b83a7e5f593b
Sarah <sarah.jehin@belgacom.net>
2016-11-04 21:04:12 +0100

    dashboard home

--- quer/resources/views/dashboard.blade.php
+++ quer/resources/views/dashboard.blade.php
@@ -9,19 +9,18 @@
 
         <h1>Welkom {{ Auth::user()->first_name }}</h1>
        
-       <div>
-           Wat kan je allemaal doen op dit dashboard?
+       <div class="dashboard">
+           <h3>Wat kan je allemaal doen op dit dashboard?</h3>
            <div>
                <ul>
-                   <li>Bij je advertenties kan je zien hoeveel Que'rs zich hebben aangemeld voor jouw advertenties.</li>
+                   <li>Bij je <a href="{{ url('/my_advertisements') }}">advertenties</a> kan je zien hoeveel Que'rs zich hebben aangemeld voor jouw advertenties.</li>
+                   <li>Of je kan er een <a href="{{ url('/add_advertisement')}}">nieuwe advertentie</a> aanmaken.</li>
+                   <li>Heb je gequed voor iemand of heb je een contract aangegaan met een quer, blijf dan op de hoogte via je <a href="{{ url('/contracts_overview') }}">contracten.</a></li>
+                   <li>Heb je zelf ook al gequed? Volg dan wat mensen van jouw vinden bij je <a href="{{ url('/user_details/'. Auth::user()->id) }}">reviews.</a></li>
                </ul>
            </div>
        </div>
-       <p>Hierop kan je je huidige contracten, openstaande advertenties, ... zien</p>
-      
-       <p>Onderstaand dashboard menu moet ook in een soort layout staan die ge-extend kan worden</p>
-
-
+       
     </div>
 
 @endsection
