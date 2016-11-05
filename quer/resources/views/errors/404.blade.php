407eae6a63601f02c21c7be60a934659f9530859
Sarah <sarah.jehin@belgacom.net>
2016-11-05 21:42:32 +0100

    404

--- quer/resources/views/errors/404.blade.php
+++ quer/resources/views/errors/404.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>404 deze webpagina is niet gevonden</h1>
+<div class="variable_content not_found">
+    <h2>404: deze webpagina is niet gevonden</h2>
+    
+    <div>
+        Sorry de pagina die je zoekt bestaat helaas niet meer...
+    </div>
+    <div>
+        Ga terug naar <a href="{{ url('/')}}">home</a>
+    </div>
+</div>
 @endsection
